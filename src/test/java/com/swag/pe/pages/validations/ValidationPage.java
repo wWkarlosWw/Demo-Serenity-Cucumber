eb1b263f4528a8e0b3e131e481779264a386c030
karlos batista leite cardozo <129088649+wWkarlosWw@users.noreply.github.com>
2026-05-27 21:52:08 -0400

    Update ValidationPage.java

--- src/test/java/com/swag/pe/pages/validations/ValidationPage.java
+++ src/test/java/com/swag/pe/pages/validations/ValidationPage.java
@@ -5,8 +5,8 @@ import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.support.FindBy;
 
 public class ValidationPage extends PageObject {
-    @FindBy(xpath = "//div[@class='product_label']")
-    protected WebElementFacade lbl_product;
+@FindBy(xpath = "//span[@class='title']")
+protected WebElementFacade lbl_product;
     @FindBy(css="h3[data-test='error']")
     protected WebElementFacade lbl_errorMessage;
 
